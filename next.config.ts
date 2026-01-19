35f051757ac63efce922fc6e511db4c01edce734
m_tantan <38578812+m-tantan@users.noreply.github.com>
2026-01-19 22:56:12 +0200

    Reduce config file

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
-// Set to true if using custom domain, false if using github.io/repo-name path
-const useCustomDomain = true;
-
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: 'export',
-  // When using a custom domain, basePath should be empty
-  // When using username.github.io/repo-name, basePath should be '/repo-name'
   basePath: '',
   images: {
     unoptimized: true,
