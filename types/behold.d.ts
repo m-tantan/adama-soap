e255e5c747d173ef1ee752a98edac21b6e3b0bfa
m_tantan <38578812+m-tantan@users.noreply.github.com>
2026-01-26 21:53:28 +0200

    I too like to live dangerously

--- types/behold.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    'behold-widget': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-      'feed-id'?: string;
-    };
-  }
-}
